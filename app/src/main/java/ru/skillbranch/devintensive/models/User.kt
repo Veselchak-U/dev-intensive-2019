7dca6708430372c480fc808396c57b8cb79fb9e5
Veselchak-U <usatov-dv@ya.ru>
2019-11-20 22:17:26 +0300

    hometask_2 task "**Паттерн Builder"

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -24,6 +24,72 @@ data class User (
 
     constructor(id: String) : this(id, firstName = "John", lastName = "Doe")
 
+    constructor(builder : Builder) : this(
+        id = builder.id,
+        firstName = builder.firstName,
+        lastName = builder.lastName,
+        avatar = builder.avatar,
+        rating = builder.rating,
+        respect = builder.respect,
+        lastVisit = builder.lastVisit,
+        isOnline = builder.isOnline
+    )
+
+    class Builder {
+        var id: String = ""
+        var firstName: String? = null
+        var lastName: String? = null
+        var avatar: String? = null
+        var rating: Int = 0
+        var respect: Int = 0
+        var lastVisit: Date? = Date()
+        var isOnline: Boolean = false
+
+        fun id(s : String) : Builder {
+            this.id = s
+            return this
+        }
+
+        fun firstName(s : String) : Builder {
+            this.firstName = s
+            return this
+        }
+
+        fun lastName(s : String) : Builder {
+            this.lastName = s
+            return this
+        }
+
+        fun avatar(s : String) : Builder {
+            this.avatar = s
+            return this
+        }
+
+        fun rating(n : Int) : Builder {
+            this.rating = n
+            return this
+        }
+
+        fun respect(n : Int) : Builder {
+            this.respect = n
+            return this
+        }
+
+        fun lastVisit(d : Date) : Builder {
+            this.lastVisit = d
+            return this
+        }
+
+        fun isOnline(b : Boolean) : Builder {
+            this.isOnline = b
+            return this
+        }
+
+        fun build() : User {
+            return User(this)
+        }
+    }
+
     init {
 //        println("It's Alive!!!\n" +
 //                "${if(lastName==="Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n")
