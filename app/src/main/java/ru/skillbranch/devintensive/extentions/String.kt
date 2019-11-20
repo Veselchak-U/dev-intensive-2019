1efe01f5d137babc1f20176bd88f837e5756ad4e
Veselchak-U <usatov-dv@ya.ru>
2019-11-20 23:17:37 +0300

    hometask_2 task "*String.stripHtml"

--- app/src/main/java/ru/skillbranch/devintensive/extentions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/String.kt
@@ -13,3 +13,14 @@ fun String.truncate(value: Int = 16) : String {
         return text
     }
 }
+
+fun String.stripHtml() : String {
+    var text : String = ""
+
+    text = this.replace("<.*?>".toRegex(), "") // replace words kind of <tag>
+    text = text.replace("&.*?;".toRegex(), "") // replace words kind of &amp;
+    text = text.replace("^ +| +\$|( )+".toRegex() , " ") // replace multiple spaces in the middle of the line
+    text = text.trim() // replace spaces at the edge of the line
+
+    return text
+}
