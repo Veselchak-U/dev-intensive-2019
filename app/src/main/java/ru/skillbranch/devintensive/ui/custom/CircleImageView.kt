c395b8b408a02828d421201258438f9825f869e1
Veselchak-U <usatov-dv@ya.ru>
2020-01-16 22:01:22 +0300

    hometask_4 CircleImageView

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -6,8 +6,11 @@ import android.util.AttributeSet
 import android.widget.ImageView
 import androidx.annotation.ColorRes
 import androidx.annotation.Dimension
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import ru.skillbranch.devintensive.R
 import kotlin.math.min
+
 
 class CircleImageView @JvmOverloads constructor(
     context: Context,
@@ -26,6 +29,7 @@ class CircleImageView @JvmOverloads constructor(
     private var circlePaint = Paint()
     private var avaPaint = Paint()
     private lateinit var avaBitmap: Bitmap
+    private var isBitmap = false //
 
     init {
         if (attrs != null) {
@@ -34,20 +38,30 @@ class CircleImageView @JvmOverloads constructor(
             borderWidth =
                 a.getDimension(R.styleable.CircleImageView_cv_borderWidth, DEFAULT_BORDER_WIDTH)
             a.recycle()
-        }
-        circlePaint.color = borderColor
 
-        // Use default (don't-resized) shader
-        avaBitmap = BitmapFactory.decodeResource(resources, R.drawable.ava_test)
-        val shader = BitmapShader(avaBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-        avaPaint.shader = shader
+            circlePaint.color = borderColor
+
+            // Checking for available the picture
+            val srcId = attrs.getAttributeResourceValue(
+                "http://schemas.android.com/apk/res/android", "src", -1
+            )
+            val drawable = AppCompatResources.getDrawable(context, srcId)
+            if (drawable != null) {
+                isBitmap = true
+                avaBitmap = drawable.toBitmap()
+                // Use default (don't-resized) shader
+//        avaBitmap = BitmapFactory.decodeResource(resources, R.drawable.avatar_default)
+                val shader = BitmapShader(avaBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+                avaPaint.shader = shader
+            }
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
         // Use resized shader if it possible
-        if (measuredWidth > 0 && measuredHeight > 0) {
+        if (isBitmap && measuredWidth > 0 && measuredHeight > 0) {
             val scaledBitmap =
                 Bitmap.createScaledBitmap(avaBitmap, measuredWidth, measuredHeight, false)
             val newShader = BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
@@ -56,19 +70,22 @@ class CircleImageView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawCircle(
-            width.toFloat() / 2.0F, height.toFloat() / 2.0F,
-            min(width, height).toFloat() / 2.0F, circlePaint
-        )
-
-        canvas?.drawCircle(
-            width.toFloat() / 2.0F, height.toFloat() / 2.0F,
-            (min(width, height).toFloat() - borderWidth) / 2.0F, avaPaint
-        )
+        if (isBitmap) {
+            canvas?.drawCircle(
+                width.toFloat() / 2.0F, height.toFloat() / 2.0F,
+                min(width, height).toFloat() / 2.0F, circlePaint
+            )
+            canvas?.drawCircle(
+                width.toFloat() / 2.0F, height.toFloat() / 2.0F,
+                (min(width, height).toFloat() - borderWidth) / 2.0F, avaPaint
+            )
+        } else {
+            super.onDraw(canvas)
+        }
     }
 
     @Dimension
-    fun getBorderWidth() = borderWidth
+    fun getBorderWidth() = borderWidth.toInt()
 
     fun setBorderWidth(@Dimension dp: Int) {
         if (dp > 0) borderWidth = dp.toFloat()
