9b5974a68c811f177b57d5163f4ca006ef830a81
Veselchak-U <usatov-dv@ya.ru>
2020-03-17 14:19:57 +0300

    hometask_5 first commit

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,8 +1,8 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(value: Int = 16): String {
-    val filler : String = "..."
-    var text : String = ""
+    val filler: String = "..."
+    var text: String = ""
 
     text = this.trim()
     if (text.length <= value)
@@ -15,11 +15,14 @@ fun String.truncate(value: Int = 16): String {
 }
 
 fun String.stripHtml(): String {
-    var text : String = ""
+    var text: String = ""
 
     text = this.replace("<.*?>".toRegex(), "") // replace words kind of <tag>
     text = text.replace("&.*?;".toRegex(), "") // replace words kind of &amp;
-    text = text.replace("^ +| +\$|( )+".toRegex() , " ") // replace multiple spaces in the middle of the line
+    text = text.replace(
+        "^ +| +\$|( )+".toRegex(),
+        " "
+    ) // replace multiple spaces in the middle of the line
     text = text.trim() // replace spaces at the edge of the line
 
     return text
--- app/src/main/java/ru/skillbranch/devintensive/models/data/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/data/User.kt
@@ -1,9 +1,10 @@
-package ru.skillbranch.devintensive.models
+package ru.skillbranch.devintensive.models.data
 
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
-data class User (
+data class User(
     val id: String,
     var firstName: String?,
     var lastName: String?,
@@ -14,6 +15,23 @@ data class User (
     val isOnline: Boolean = false
 
 ) {
+    fun toUserItem(): UserItem {
+        val lastActivity = when {
+            lastVisit == null -> "Ещё ни разу не заходил"
+            isOnline -> "В сети"
+            else -> "Последний раз был ${lastVisit.humanizeDiff()}"
+        }
+
+        return UserItem(
+            id,
+            "${firstName.orEmpty()} ${lastName.orEmpty()}",
+            Utils.toInitials(firstName, lastName),
+            avatar,
+            lastActivity,
+            false,
+            isOnline
+        )
+    }
 
     constructor(id: String, firstName: String?, lastName: String?) : this(
         id = id,
@@ -24,7 +42,7 @@ data class User (
 
     constructor(id: String) : this(id, firstName = "John", lastName = "Doe")
 
-    constructor(builder : Builder) : this(
+    constructor(builder: Builder) : this(
         id = builder.id,
         firstName = builder.firstName,
         lastName = builder.lastName,
@@ -45,64 +63,63 @@ data class User (
         var lastVisit: Date? = Date()
         var isOnline: Boolean = false
 
-        fun id(s : String) : Builder {
+        fun id(s: String): Builder {
             this.id = s
             return this
         }
 
-        fun firstName(s : String) : Builder {
+        fun firstName(s: String): Builder {
             this.firstName = s
             return this
         }
 
-        fun lastName(s : String) : Builder {
+        fun lastName(s: String): Builder {
             this.lastName = s
             return this
         }
 
-        fun avatar(s : String) : Builder {
+        fun avatar(s: String): Builder {
             this.avatar = s
             return this
         }
 
-        fun rating(n : Int) : Builder {
+        fun rating(n: Int): Builder {
             this.rating = n
             return this
         }
 
-        fun respect(n : Int) : Builder {
+        fun respect(n: Int): Builder {
             this.respect = n
             return this
         }
 
-        fun lastVisit(d : Date) : Builder {
+        fun lastVisit(d: Date): Builder {
             this.lastVisit = d
             return this
         }
 
-        fun isOnline(b : Boolean) : Builder {
+        fun isOnline(b: Boolean): Builder {
             this.isOnline = b
             return this
         }
 
-        fun build() : User {
+        fun build(): User {
             return User(this)
         }
     }
 
-    init {
-//        println("It's Alive!!!\n" +
-//                "${if(lastName==="Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}\n")
-    }
-
     companion object Factory {
-        private var lastId : Int = -1
+        private var lastId: Int = -1
 
-        fun makeUser(fullName: String?) : User {
+        fun makeUser(fullName: String?): User {
             lastId++
 
             val (firstName, lastName) = Utils.parseFullName(fullName)
-            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+            return User(
+                id = "$lastId",
+                firstName = firstName,
+                lastName = lastName
+            )
         }
     }
 
