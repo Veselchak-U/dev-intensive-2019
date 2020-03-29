f3df5bfc133dc8970e76156ad708c3fcf60a943a
Veselchak-U <usatov-dv@ya.ru>
2020-03-29 11:26:23 +0300

    hometask_5 after change project structure

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -1,24 +1,20 @@
 package ru.skillbranch.devintensive.extensions
 
-import ru.skillbranch.devintensive.models.UserView
-import ru.skillbranch.devintensive.models.data.User
-import ru.skillbranch.devintensive.utils.Utils
-
-fun User.toUserView(): UserView {
-    val nickName = Utils.transliteration("$firstName $lastName")
-    val initials = Utils.toInitials(firstName, lastName)
-    val status = if(lastVisit == null) "Ещё ни разу не был"
-                       else if(isOnline) "В сети"
-                       else "Был: ${lastVisit.humanizeDiff()}"
-
-    return UserView(
-        id,
-        fullName = "$firstName $lastName",
-        nickName = nickName,
-        initials = initials,
-        avatar = avatar,
-        status = status
-    )
-}
+//fun User.toUserView(): UserView {
+//    val nickName = Utils.transliteration("$firstName $lastName")
+//    val initials = Utils.toInitials(firstName, lastName)
+//    val status = if(lastVisit == null) "Ещё ни разу не был"
+//                       else if(isOnline) "В сети"
+//                       else "Был: ${lastVisit.humanizeDiff()}"
+//
+//    return UserView(
+//        id,
+//        fullName = "$firstName $lastName",
+//        nickName = nickName,
+//        initials = initials,
+//        avatar = avatar,
+//        status = status
+//    )
+//}
 
 
